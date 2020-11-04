a86fe57dd6868e1a1e7bb2e1ef1adf68358bf23d
TylerLyczak <tyler.m.lyczak@gmail.com>
2020-11-03 20:23:32 -0600

    Initial Commit

---
+++ Switch View Controller Based On UserDefaultsTests/Switch_View_Controller_Based_On_UserDefaultsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Switch_View_Controller_Based_On_UserDefaultsTests.swift
+//  Switch View Controller Based On UserDefaultsTests
+//
+//  Created by Tyler Lyczak on 11/3/20.
+//
+
+import XCTest
+@testable import Switch_View_Controller_Based_On_UserDefaults
+
+class Switch_View_Controller_Based_On_UserDefaultsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
